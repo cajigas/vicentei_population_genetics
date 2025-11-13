31582c59c8e8ec30ae9ec2e712ab8df5c5ddb93a
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2025-11-13 17:56:30 +0100

    Refactor data import and clean up comments
    Removed redundant sample sheet loading and updated comments.

--- 2. Population genetics.R
+++ 2. Population genetics.R
@@ -17,14 +17,14 @@ library(adegenet)
 library(vcfR)
 library (LEA)
 
-# Import data
+# Load sample sheet with populations information
+samples<-read.csv("populations.ingroup_vicentei.tab", sep="\t",header=T)
+
+# Import data: the input PLINK file is available in Zenodo, please find the link in the "Data availability" statement in the publication
 importdata(inputprefix="vicentei_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.nonCoding.LDpruned.PLINK",
            samplefile="populations.ingroup_vicentei.tab", pop_order=c("CAL","CEI","EMP","LOM"), 
            colourvector=c("#fc4d4d","#54c099","#964B00","#84f084"),
            geofile="geofile_vicentei.txt", do_citations=FALSE)
-
-# Load sample sheet with populations information
-samples<-read.csv("populations.ingroup_vicentei.tab", sep="\t",header=T)
 
 # Retain al data because it was previously filtered in GATK and PLINK)
 filterdata(indmiss=1,snpmiss=1,min_mac=0,dohefilter=FALSE,min_spacing=0,
@@ -35,5 +35,3 @@ calcdiversity(nrsites=NULL, do_sfs=FALSE,do_venn=FALSE)
 LEAce(min_demes=1,max_demes=4,runanalysis=TRUE,nruns=50,export="pdf")
 calcdistance(nchroms=NULL, dodistgenpop=FALSE)
 inferdemography(do_LEA=TRUE,Kmin=1,Kmax=4,do_f3=TRUE,f3_preparefiles=FALSE,jk_blocksize=1000)
-
-
