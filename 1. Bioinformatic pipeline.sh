5ad69d68559863ff3e83480f672d2c709d7e882e
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2026-04-16 16:02:45 +0200

    Revise pipeline script with updated date and VCF steps
    Updated last modified date and added details for VCF postprocessing steps specific to Oophaga vicentei.

--- 1. Bioinformatic pipeline.sh
+++ 1. Bioinformatic pipeline.sh
@@ -1,9 +1,9 @@
 ### Bioinformatic pipeline followed to do haplotype calling of Oophaga vicentei RNA reads
-# when using the reference genome of Oophaga sylvatica ###
+# when using the reference genome of Oophaga sylvatica (NCBI accession: GCA_033576555.1) ###
 
 Author: Anaisa Cajigas Gandia
 License: GNU
-Last updated: 30.10.2025
+Last updated: 16.04.2026
 
 Main steps included:
 1- RNA reads quality trimming
@@ -27,3 +27,39 @@ to find the scripts used for this analysis.
 Note that function and software settings were the same for both species, 
 but the number and name of samples, as well as threads varied accordingly. 
 For any specific question, contact me directly. 
+
+### The only different step is VCF postprocessing, and for O. vicentei I show the script below:
+
+cd .../vicentei/population_genetics/VCF
+
+# Filter for minimum depth, minor allele count and missing data
+vcftools --vcf vicentei_GATK_variants.filtered.biallelic.SNPs.vcf \
+--minDP 10 --mac 3 --max-missing 0.75 --bed non-coding_OopSyl_NCBI_genome.bed \
+--out vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding \
+--recode --keep-INFO-all
+# After filtering, kept 50320 out of 3856452 possible sites
+
+# Remove loci in LD with PLINK
+plink2 --vcf vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.recode.vcf \
+--allow-extra-chr --set-all-var-ids @:#\$r,\$a --indep-pairwise 50 1 0.7 --bad-ld 
+# After filtering, kept 26296 SNPs  
+
+plink2 --vcf vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.recode.vcf \
+--allow-extra-chr --set-all-var-ids @:#\$r,\$a --exclude plink2.prune.out --make-bed --recode \
+--out vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK
+
+# Export in different formats for further analyses
+plink --bfile vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK \
+--allow-extra-chr --recode A --out vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK
+
+plink --bfile vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK \
+--allow-extra-chr --recode --tab --out vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK
+
+plink2 --bfile vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK \
+--allow-extra-chr --freq
+
+plink2 --bfile vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK \
+--allow-extra-chr --make-rel square --read-freq plink2.afreq
+
+plink2 --bfile vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK \
+--allow-extra-chr --export vcf --out vicentei_GATK_variants.filtered.biallelic.SNPs.minDP10mac3maxmissing075.nonCoding.LDpruned.PLINK
